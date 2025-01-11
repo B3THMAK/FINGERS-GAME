424275a22510e3207e68e388a5e85543f5edb2c2
B3THMAK <132462124+B3THMAK@users.noreply.github.com>
2025-01-11 03:54:17 +0300

    player side gameprotocol

---
+++ IDEAS/GameProtocol.cs for  client side.cs
@@ -0,0 +1,24 @@
+﻿namespace GameClient
+{
+    public class GameMessage
+    {
+        public string Type { get; set; }     
+        public string Content { get; set; }     
+        public GameMove? PlayerMove { get; set; }
+        public int? Score { get; set; }          
+    }
+
+    public enum GameMove
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    public enum GameResults
+    {
+        Win,
+        Lose,
+        Draw
+    }
+}
